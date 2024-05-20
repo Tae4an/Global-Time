b3997831c73700d8e6b72961fed94bae5e0f160c
Choi TaeSan <xotks7524@gmail.com>
2024-05-20 22:20:29 +0900

    Feat: 프론트엔드 접근을 위한 CORS 설정 추가

--- src/main/java/org/example/infrastructure/config/WebConfig.java
+++ src/main/java/org/example/infrastructure/config/WebConfig.java
@@ -4,6 +4,7 @@ import org.example.application.security.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -20,5 +21,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://10.20.34.151:3000")  // 백엔드 IP 주소와 프론트엔드 포트
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
