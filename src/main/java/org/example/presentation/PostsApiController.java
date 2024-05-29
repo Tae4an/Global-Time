d18cddb7d3355d56fb36f02f581c7f94af8acaa2
Choi TaeSan <xotks7524@gmail.com>
2024-05-29 20:04:40 +0900

    Fix: search 메서드 매핑 추가 및 페이징 처리 수정
    - /api/posts 경로에 대한 GET 요청을 처리하는 search 메서드를 추가.
    - 페이지네이션 및 키워드 검색 기능을 PostsService와 연동하여 동작하도록 수정.

--- src/main/java/org/example/presentation/PostsApiController.java
+++ src/main/java/org/example/presentation/PostsApiController.java
@@ -2,45 +2,29 @@ package org.example.presentation;
 
 import org.example.application.PostsService;
 import org.example.application.dto.PostsDto;
-import org.example.application.dto.UserDto;
-import org.example.application.security.auth.LoginUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-/**
- * REST API Controller
- */
-@RequestMapping("/api")
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/posts")
 public class PostsApiController {
 
     private final PostsService postsService;
 
-    /* CREATE */
-    @PostMapping("/posts")
-    public ResponseEntity save(@RequestBody PostsDto.Request dto, @LoginUser UserDto.Response user) {
-        return ResponseEntity.ok(postsService.save(dto, user.getNickname()));
+    @GetMapping
+    public ResponseEntity<Page<PostsDto.Response>> list(@RequestParam(value = "page", defaultValue = "0") int page,
+                                                        @RequestParam(value = "keyword", defaultValue = "") String keyword,
+                                                        Pageable pageable) {
+        Page<PostsDto.Response> postsPage = postsService.search(keyword, pageable);
+        return ResponseEntity.ok(postsPage);
     }
 
-    /* READ */
-    @GetMapping("/posts/{id}")
-    public ResponseEntity read(@PathVariable Long id) {
-        return ResponseEntity.ok(postsService.findById(id));
-    }
-
-    /* UPDATE */
-    @PutMapping("/posts/{id}")
-    public ResponseEntity update(@PathVariable Long id, @RequestBody PostsDto.Request dto) {
-        postsService.update(id, dto);
-        return ResponseEntity.ok(id);
-    }
-
-    /* DELETE */
-    @DeleteMapping("/posts/{id}")
-    public ResponseEntity delete(@PathVariable Long id) {
-        postsService.delete(id);
-        return ResponseEntity.ok(id);
+    @PostMapping
+    public ResponseEntity<Long> save(@RequestBody PostsDto.Request dto, @RequestParam("nickname") String nickname) {
+        return ResponseEntity.ok(postsService.save(dto, nickname));
     }
 }
