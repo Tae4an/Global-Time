3f51438ff96147a165743c3f73e26fab3f407dca
Choi TaeSan <xotks7524@gmail.com>
2024-06-10 15:20:27 +0900

    Refactor: 사용자 컨텍스트 기능 추가
    - Header 컴포넌트에서 useUser 훅을 사용하여 사용자 정보를 가져오도록 수정
    - 기존의 a 태그를 Link 컴포넌트로 변경하여 라우팅을 원활하게 처리
    - handleLogout 함수를 추가하여 로그아웃 시 사용자 상태를 초기화하고 로그인 페이지로 리다이렉트
    - 'Board Service' 제목을 'Global Time'으로 변경
    - 로그인 및 회원가입 버튼을 a 태그에서 Link 컴포넌트로 변경
    - 불필요한 props 제거 및 코드 정리

--- reactfront/src/layout/Header.js
+++ reactfront/src/layout/Header.js
@@ -3,12 +3,20 @@ import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import '../App.css';
+import { useUser } from '../context/UserContext';
 
-const Header = ({ user }) => {
+const Header = () => {
+    const { user, setUser } = useUser();
+
+    const handleLogout = () => {
+        setUser(null); // 로그아웃 시 사용자 상태 초기화
+        window.location.href = '/auth/login'; // 로그아웃 후 로그인 페이지로 이동
+    };
+
     return (
         <>
             <div id="header" className="d-flex bd-highlight">
-                <Link to="/" className="p-2 flex-grow-1 bd-highlight">Board Service</Link>
+                <Link to="/" className="p-2 flex-grow-1 bd-highlight">Global Time</Link>
                 <form action="/posts/search" method="GET" className="form-inline p-2 bd-highlight" role="search">
                     <input type="text" name="keyword" className="form-control" id="search" placeholder="검색" />
                     <button className="btn btn-success bi bi-search" type="submit"></button>
@@ -20,13 +28,13 @@ const Header = ({ user }) => {
                     {user ? (
                         <>
                             <span className="mx-3">{user.nickname}님 안녕하세요!</span>
-                            <a href="/logout" className="btn btn-outline-dark">로그아웃</a>
-                            <a href="/modify" className="btn btn-outline-dark bi bi-gear"></a>
+                            <button onClick={handleLogout} className="btn btn-outline-dark">로그아웃</button>
+                            <Link to="/auth/modify" className="btn btn-outline-dark bi bi-gear"></Link>
                         </>
                     ) : (
                         <>
-                            <a href="/auth/login" role="button" className="btn btn-outline-dark bi bi-lock-fill"> 로그인</a>
-                            <a href="/auth/join" role="button" className="btn btn-outline-dark bi bi-person-circle"> 회원가입</a>
+                            <Link to="/auth/login" role="button" className="btn btn-outline-dark bi bi-lock-fill"> 로그인</Link>
+                            <Link to="/auth/join" role="button" className="btn btn-outline-dark bi bi-person-circle"> 회원가입</Link>
                         </>
                     )}
                 </div>
