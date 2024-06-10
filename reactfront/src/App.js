d668edc8f139577d6ef99cde0b47f744f965ec59
Choi TaeSan <xotks7524@gmail.com>
2024-06-10 15:16:44 +0900

    Feat: 사용자 컨텍스트 추가 및 페이지 라우팅 수정
    - UserProvider를 App 컴포넌트에 추가하여 사용자 컨텍스트를 제공하도록 수정
    - IndexList 컴포넌트를 사용하여 게시글 목록을 표시하도록 라우팅 추가
    - AdminPage 컴포넌트 라우팅 추가
    - 기존 PostList, PostWrite, PostUpdate, PostRead, UserModify, UserJoin, UserLogin 컴포넌트 라우팅 유지
    - 초기화 작업 제거 및 관련 불필요한 코드 정리

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
 import PostList from './posts/PostList';
 import PostWrite from './posts/PostCreate';
 import PostUpdate from './posts/PostUpdate';
@@ -9,26 +10,33 @@ import UserJoin from './user/UserJoin';
 import UserLogin from './user/Login';
 import Header from './layout/Header';
 import Footer from './layout/Footer';
+import AdminPage from "./admin/AdminPage";
+import IndexList from "./IndexList";
 
 const App = () => {
     useEffect(() => {
-        // 초기화 작업이 필요할 경우 여기에 추가
+
     }, []);
 
     return (
+        <UserProvider>
         <Router>
             <Header />
             <Routes>
-                <Route exact path="/" element={<PostList />} />
+                <Route exact path="/" element={<UserLogin />} />
+                <Route path="/admin/user" element={<AdminPage />} />
+                <Route path="/posts/list" element={<PostList />} />
                 <Route path="/posts/write" element={<PostWrite />} />
                 <Route path="/posts/update/:id" element={<PostUpdate />} />
                 <Route path="/posts/read/:id" element={<PostRead />} />
+                <Route path="/posts" element= {<IndexList/>}/>
                 <Route path="/auth/modify" element={<UserModify />} />
                 <Route path="/auth/join" element={<UserJoin />} />
                 <Route path="/auth/login" element={<UserLogin />} />
             </Routes>
             <Footer />
         </Router>
+        </UserProvider>
     );
 };
 
